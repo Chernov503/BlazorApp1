aee6b6ebcf041d1a52747d1b86f2cf2a6b5d396a
hernov503 <chernov-viktor@internet.ru>
2023-10-03 11:22:24 +0400

    Добавьте файлы проекта.

---
+++ Logic/XMLReader.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+
+namespace BlazorApp1.Logic
+{
+    public class XMLReader
+    {
+        public XElement e { get; set; }
+
+        public List<XMLReader> eChilds = new List<XMLReader>();
+
+        public List<XAttribute> a;
+        public XMLReader(XElement e)
+        {
+            this.e = e;
+            foreach(var el in this.e.Elements().ToList())
+            {
+                eChilds.Add(new XMLReader(el));
+            }
+            a = e.Attributes().ToList();
+        }
+    }
+}
